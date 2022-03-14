60c5eb4e0dc05570203d6115cf38b8ce6d3ea555
Herry_robot <744475118@qq.com>
2022-03-14 22:55:38 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='rl_rvo_nav',
+    py_modules=['rl_rvo_nav'],
+    version= '2.0',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'transforms3d',
+        'gym',
+        'Pathlib'
+    ],
+    description="source code of the paper",
+    author="Han Ruihua",
+)
